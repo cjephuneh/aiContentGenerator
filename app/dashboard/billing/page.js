4f191e3d5857b9b8c512fe5a27bb6ad07970a93f
cjephuneh <calebjephuneh@gmail.com>
2024-08-08 15:05:23 +0300

    chore: Update billing page layout for monthly plan upgrade

--- app/dashboard/billing/page.js
+++ app/dashboard/billing/page.js
@@ -103,7 +103,7 @@ function billing() {
       <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
         <h2 className="text-center font-bold text-3xl my-3">Upgrade With Monthly Plan</h2>
 
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8 ">
           <div className="rounded-2xl bg-white border border-gray-200 p-6 shadow-sm sm:px-8 lg:p-12">
             <div className="text-center">
               <h2 className="text-lg font-medium text-gray-900">
